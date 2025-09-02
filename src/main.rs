f3b429f9a689b32eab9a20343db5fed8548a637f
xonx4l <sk797119@gmail.com>
2025-09-02 23:58:25 +0530

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-// code here 
+use axum::{ routing::{post,get},Router,http::status};
+use std::sync::Arc;
+
+
+async fn main() -> Result<() , Box<dyn std::error::Error>> {
+
+    tracing_subscriber::registry()
+    .init();
+}
